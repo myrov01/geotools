55f9238ee9c91646291436540f0a59505d6a2312
AleksandrMyrov <myrovaleksandr49@gmail.com>
2024-11-15 19:57:27 +0300

    Read gpx file

--- tracks/track_lenght.py
+++ tracks/track_lenght.py
@@ -1,9 +1,23 @@
 import geopandas as gpd
+import gpxpy
+
+
+def load_gpx_as_geodataframe(gpx_file_path):
+    with open(gpx_file_path, 'r') as gpx_file:
+        gpx = gpxpy.parse(gpx_file)
+
+    lines = []
+
+    print(gpx.tracks)
+    return lines
 
 
 if __name__ == "__main__":
-    # gdf = gpd.read_file("./track_example.gpx", layer='tracks')
+    # gpx_file = gpd.read_file("../track_example.gpx")
 
-    geometry = gdf["geometry"]
+    gpx_file_path = "../track_example.gpx"
 
-    print(geometry)
+    gdf_lines = load_gpx_as_geodataframe(gpx_file_path)
+
+    print("\nTracks:")
+    print(gdf_lines)
