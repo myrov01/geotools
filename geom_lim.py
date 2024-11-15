443c694ff01550176bc746c6f3cb02b7f9972cdb
AleksandrMyrov <myrovaleksandr49@gmail.com>
2024-11-15 19:57:27 +0300

    Read gpx file

--- geom_lim.py
+++ geom_lim.py
@@ -4,18 +4,30 @@ from geom_calc import geom_calc
 
 if __name__ == "__main__":
     gdf = gpd.read_file("./vegetation-polygon.gpkg")
-
     geometry = gdf["geometry"]
 
+    point_counts = []
     total_points = 0
+    max_poligon_points = 0
 
     for geom in geometry:
         point_count = geom_calc(geom)
+        point_counts.append(point_count)
         total_points += point_count
 
-        threshold_value = 500
+        if max_poligon_points < point_count:
+            max_poligon_points = point_count
 
+        threshold_value = 500
         if point_count >= threshold_value:
             print(f"The number of points in the polygon: {point_count}")
 
-    print(f"Total points: {total_points}")
+    sorted_list = point_counts[:]
+    sorted_list.sort()
+
+    three_max_poligon_points = sorted_list[-3:]
+    for index, count in enumerate(three_max_poligon_points):
+        print(f"\nPolygon {index} with {count} points")
+
+    print(f"\nMax poligon points: {max_poligon_points}")
+    print(f"\nTotal points: {total_points}")
